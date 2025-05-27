36eb1e1b572ab12815f28cef3af9931ce3fc397a
Alain Sosa Velarde <alain.sosa.v@gmail.com>
2025-05-27 15:42:29 -0400

    proyecto finalisado

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,9 +1,5 @@
-export default function Guitar({ guitar, setCart }) {
-  const { ed, name, price, description, image } = guitar;
-
-  const handleClick = (guitar) => {
-    setCart(guitar);
-  };
+export default function Guitar({ guitar, addToCart }) {
+  const { name, price, description, image } = guitar;
 
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -21,7 +17,7 @@ export default function Guitar({ guitar, setCart }) {
         <button
           type="button"
           className="btn btn-dark w-100"
-          onClick={() => handleClick(guitar)}
+          onClick={() => addToCart(guitar)}
         >
           Agregar al Carrito
         </button>
